025dee7d49faf2994ed8c7079807cd3acf00bd91
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2022-11-11 22:28:22 +0200

    add something

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { User } from "./socialProfile/userProfile";
+import user from "./socialProfile/user.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <User user={user}/>
+    </>
   );
 };
+
